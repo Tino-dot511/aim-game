1ccdb40e54be090807eac83efa23af8b8ad7ffd2
Tino-dot511 <bustossilva824@gmail.com>
2026-02-08 20:00:36 +0800

    Comments and Class description

--- Pick.py
+++ Pick.py
@@ -1,4 +1,8 @@
-#This class is used to display the amount of seconds you choose to play the game for.
+'''
+This class displays is used to display an input screen to choose the amount of seconds
+you play the game for, validate the input so that it can be used in the square class, as well as 
+transitioning to the game aspect with the squares.
+'''
 
 import Square
 class Pick():
@@ -9,6 +13,8 @@ class Pick():
         self.seconds = None
 
     def check_type(self,value,type):
+        # Returns true or false to validate that the type of the unit entered can be converted.
+        # Primarily used to avoid errors in the case that someone accidently inputs a letter instead of a number.
         try:
             type(value)
             return True
@@ -16,6 +22,7 @@ class Pick():
             return False
 
     def on_enter_seconds(self,event,enter_sec):
+            # Gets the input entered by the player to check if it is a number to then be used as an argument in the square class.
             self.seconds =  enter_sec.get()
             if self.check_type(self.seconds,float):
                 self.root.unbind('<Escape>')
@@ -24,6 +31,7 @@ class Pick():
                 game_screen = Square.Square(self.tk,self.root,self.seconds)
                 game_screen.game_screen()
             else:
+                # Displays a message to try again if the input by the player isn't a number
                  try_again = self.tk.Label(self.canvas,text='Please put in a number', font=('Arial',20), fg='black')
                  try_again.place(x = (self.root.winfo_screenwidth()/2)-150, y = (self.root.winfo_screenheight()/4))
             
@@ -35,10 +43,11 @@ class Pick():
         self.canvas.pack(fill=self.tk.BOTH, expand=True)
         time_label = self.tk.Label(self.canvas,text='How many seconds would you like to play?', font=('Arial',20), fg='black')
         time_label.place(x = (self.root.winfo_screenwidth()/2)-250, y = (self.root.winfo_screenheight()/3))
-        enter_sec = self.tk.Entry(self.canvas,font=('Arial',20))
+        enter_sec = self.tk.Entry(self.canvas,font=('Arial',20))# The box used by the player to input an amount of seconds
         enter_sec.place(x = (self.root.winfo_screenwidth()/2)-150, y = (self.root.winfo_screenheight()/2))
         label_esc = self.tk.Label(self.canvas,text=' press esc to leave', font=('Arial',12), fg='white',bg='black')
         label_esc.place(x = 0,y = self.root.winfo_screenheight() - 20)
-        enter_sec.bind('<Return>',lambda event:self.on_enter_seconds(event,enter_sec))
+        enter_sec.bind('<Return>',lambda event:self.on_enter_seconds(event,enter_sec))# When the enter key is pressed, it will access the on_enter_seconds function
 
-        self.root.bind('<Escape>',self.esc_exit)
+        self.root.bind('<Escape>',self.esc_exit)# Escape button to leave the game early
+
