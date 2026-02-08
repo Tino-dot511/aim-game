fa6edb71bb2978bf6e858d6499fc9cdadacc651d
Tino-dot511 <bustossilva824@gmail.com>
2026-02-08 23:07:31 +0800

    Comments and Class description

--- Square.py
+++ Square.py
@@ -1,3 +1,9 @@
+'''
+This class runs the actual game where the player clicks the square. This class deals 
+with creating coordinates for the squares, and using overlapping logic, ensures the
+squares don't overlap in game, then creates the squares and displays them for the game.
+'''
+
 import time
 import random as ran
 class Square():
@@ -7,35 +13,42 @@ class Square():
         self.tim = time
         self.canvas = None
         self.stopwatch_start = None
-        self.dict = {}
-        self.lst = []
-        self.hits = []
-        self.shots = []
+        self.dict = {} # Dictionary of square with their coordinates
+        self.lst = [] # List to save coordinates
+        self.hits = [] # How many squares have been hit 
+        self.shots = [] # How many shots/clicks the player has done
     
     def hits_or_not(self,x,y):
+        # Checks if squares have been hit
         dict_copy = self.dict.copy()
         for key,items in dict_copy.items():
             if (x >= items[0] and x <= items[0]+50) and (y >= items[1] and y <= items[1]+50):
+                # If a square is hit
                 self.lst.remove(items)
-                self.canvas.delete(key)
-                del self.dict[key]
-                self.check_coordinates()
+                self.canvas.delete(key) 
+                del self.dict[key] 
+                self.check_coordinates() # New square coordinate is generated and checked for overlapping
                 rect = self.canvas.create_rectangle(self.lst[-1][0],self.lst[-1][1],self.lst[-1][0]+50,self.lst[-1][1]+50,fill='red')
+                # Square is created then added to the dictionary
                 self.dict[rect] = self.lst[-1]
                 self.hits.append(1)
 
     def stop_watch(self):
+        # Checks time every millisecond to make sure that game time lasts for as long as the time chosen in previous class
         accuracy = 0
         stopwatch_end = time.time()
-        time_gone = stopwatch_end-self.stopwatch_start
-        if time_gone >= float(self.tim):
-            self.canvas.delete('all')
-            self.canvas.unbind('<Button-1>')
+        time_gone = stopwatch_end-self.stopwatch_start # Checks the in-game time
+        if time_gone >= float(self.tim): 
+            self.canvas.delete('all') # The game stops and removes all squares
+            self.canvas.unbind('<Button-1>') # Unbinds the left-click button so the click doesn't impact shots/clicks after the game has ended
+            # Labels that display time of game, how many shots/clicks the player made, how many squares were hit, how fast they were clicking(fire rate), and accuracy.
             label_time_taken = self.tk.Label(self.canvas,text=f'Time Taken: {time_gone:.2f} seconds',font=('Arial',12), fg='black')
             label_shots = self.tk.Label(self.canvas, text=f'Shots: {len(self.shots)}', font=('Arial',12), fg='black')
             label_hits = self.tk.Label(self.canvas, text=f'Hits: {len(self.hits)}', font=('Arial',12), fg='black')
             label_fire_rate = self.tk.Label(self.canvas, text=f'Fire Rate : {(len(self.shots)/time_gone):.2f}', font=('Arial',12), fg='black')
             if len(self.shots) == 0:
+                # If player didn't take shots/click, accuracy is zero
+                # This avoids zero-division errors for accuracy calculations
                 accuracy = 0
             else:
                 accuracy = (len(self.hits)/len(self.shots))*100
@@ -49,49 +62,62 @@ class Square():
         self.canvas.after(1,self.stop_watch)
 
     def on_click(self,event):
+        # When the player clicks, the function runs and shots/click adds to its list of shots/clicks done by the player
         self.hits_or_not(event.x,event.y)
         self.shots.append(1)
 
     def esc_exit(self,event):
+        # Exits game if players presses escape key
         self.root.destroy()
 
     def check_area(self,x1,x2,y1,y2):
+        # Checks that coordinates generated don't overlap 
         flag = True
         if (x1 < x2+50 or x2 < x1+50 ) and (y1 < y2+50 or y2 < y1+50):
+            # If the start of x-axis of square 1 is less than the end of x-axis square 2, or vice versa
+            # If the start of y-axis of square 1 is less than the end of y-axis square 2, or vice versa
             flag = False
         return flag
     
     def check_coordinates(self):
+        # Generates coordinates and checks for overlapping coordinates
         while True:
             valid = True
             new_x = ran.randint(self.root.winfo_x(),self.root.winfo_screenwidth()-50)
             new_y = ran.randint(self.root.winfo_y(),self.root.winfo_screenheight()-50)
+            # Generates random numbers so squares appear in unpredictable places
             if len(self.lst) == 8:
+                # When the list reaches the max amount of squares for the game
                 break
             if len(self.lst) == 0:
-                self.lst.append([new_x,new_y])
+                self.lst.append([new_x,new_y]) 
+                # Appends the coordinates at beginning so there is no error for the for loop and because there is no overlap with one set of coordinates
                 continue
             else:
                 for i in range(len(self.lst)):
+                    # Loops through the list to check if coordinates overlap with previous coordinates in the list
                     x2,y2 = self.lst[i][0],self.lst[i][1]
                     if len(self.lst) == 8:
                         return
                     if self.check_area(new_x,x2,new_y,y2):
+                        # If new coordinates overlap with any coordinate in the list, they aren't appended to the list
                         valid = False
                 if valid:
                     self.lst.append([new_x,new_y])
 
     def game_screen(self):
-        self.check_coordinates()
-        self.canvas = self.tk.Canvas(self.root)
+        self.check_coordinates() # Generates and checks coordinates for the square
+        self.canvas = self.tk.Canvas(self.root) # Creates the canvas for the game to make the squares that are clicked
         self.canvas.pack(fill=self.tk.BOTH, expand=True)
-        for i in range(len(self.lst)):
+        for i in range(len(self.lst)): 
+            # Creates the square using the coordinates in the list and adds them to the dictionary of squares with their coordinates
             rect = self.canvas.create_rectangle(self.lst[i][0],self.lst[i][1],self.lst[i][0]+50,self.lst[i][1]+50,fill='red')
             self.dict[rect] = self.lst[i]
         label_esc = self.tk.Label(self.canvas,text=' press esc to leave', font=('Arial',12), fg='white',bg='black')
         label_esc.place(x = 0,y = self.root.winfo_screenheight() - 20)
-        self.stopwatch_start = time.time()
-        self.canvas.bind('<Button-1>',self.on_click)
-        self.root.bind('<Escape>',self.esc_exit)
-        self.stop_watch()
+        self.stopwatch_start = time.time() # Starts the timer to check the how long the game is
+        self.canvas.bind('<Button-1>',self.on_click) # If the left-click button is clicked, the on_click function will run
+        self.root.bind('<Escape>',self.esc_exit) # When escape key is pressed, the player leaves the game
+        self.stop_watch() # Runs the function in background to check how long the game goes
+
         
