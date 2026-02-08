331dd506f839e599c6c15ddf2f30e58e625bd129
Tino-dot511 <bustossilva824@gmail.com>
2026-02-08 23:09:33 +0800

    Comments and Class description

--- test_run_aim_game.py
+++ test_run_aim_game.py
@@ -1,8 +1,12 @@
+'''
+This is an example of how the game would be run using the classes
+'''
+
 import tkinter as tk
 import Start
 
-root = tk.Tk()
-root.attributes('-fullscreen', True)
-start_game = Start.Start(tk,root)
+root = tk.Tk() # Root is created first to then be used as argument for the classes
+root.attributes('-fullscreen', True) # Allows for the game to be in fullscreen the whole time
+start_game = Start.Start(tk,root) 
 
-start_game.start_screen()
+start_game.start_screen() # Starts the game
